7556cd97484a8f3355c516a2d3e291e69ca286e3
JorgeBH9 <jorgebh9@outlook.com>
2018-09-19 14:57:28 -0500

    This is part of the login already working

--- server.js
+++ server.js
@@ -3,6 +3,10 @@ var express = require("express");
 var bodyParser = require("body-parser");
 var exphbs = require("express-handlebars");
 
+var session = require("express-session");
+// Requiring passport as we've configured it
+var passport = require("./config/passport");
+
 var db = require("./models");
 
 var app = express();
@@ -12,6 +16,17 @@ var PORT = process.env.PORT || 3000;
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(express.static("public"));
+
+// We need to use sessions to keep track of our user's login status
+app.use(session({ secret: "keyboard cat", resave: true, saveUninitialized: true }));
+app.use(passport.initialize());
+app.use(passport.session());
+//
+
+// Requiring our routes
+require("./routes/htmlRoutes.js")(app);
+require("./routes/apiRoutes.js")(app);
+//
 
 // Handlebars
 app.engine(
@@ -35,8 +50,8 @@ if (process.env.NODE_ENV === "test") {
 }
 
 // Starting the server, syncing our models ------------------------------------/
-db.sequelize.sync(syncOptions).then(function() {
-  app.listen(PORT, function() {
+db.sequelize.sync(syncOptions).then(function () {
+  app.listen(PORT, function () {
     console.log(
       "==> 🌎  Listening on port %s. Visit http://localhost:%s/ in your browser.",
       PORT,
